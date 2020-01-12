2753d136bc78eff942ab51e769a2b65ff70637dd
AbrahamGeorgiadis <georgiadis.ab@gmail.com>
2020-01-12 23:01:14 +0100

    Working on the Level abstraction and Manager Singleton.

---
+++ LevelSelection/Assets/Scripts/Level.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public enum LevelStatus
+    {
+        Locked=0,
+        Available=1,
+        Completed=2
+    }
+    #region Attributes
+
+    private readonly int _id;
+    private readonly string _levelName;
+    private int _ammountOfStars;
+    private LevelStatus _status;
+
+    public int Id { get { return _id; } }
+    public string LevelName { get { return _levelName; } }
+    public int AmmountOfStars { get { return _ammountOfStars; } }
+    public LevelStatus Status { get { return _status; } }
+
+    #endregion
+
+
+    public Level(int newID, string newLevelName, int newAmmountOfStars, LevelStatus newLevelStatus)
+    {
+        this._id = newID;
+        this._levelName = newLevelName;
+        this._ammountOfStars = newAmmountOfStars;
+        this._status = newLevelStatus;
+    }
+
+    public Level(int newID, string newLevelName)
+    {
+        this._id = newID;
+        this._levelName = newLevelName;
+        this._ammountOfStars = 0;
+        this._status = LevelStatus.Locked;
+    }
+
+
+    public void CompleteLevel()
+    {
+        this._ammountOfStars = 1;
+        this._status = LevelStatus.Completed;
+    }
+    public void CompleteLevel(int ammountOfStars)
+    {
+        this._ammountOfStars = ammountOfStars;
+        this._status = LevelStatus.Completed;
+    }
+
+    public void UnlockLevel()
+    {
+        this._status = LevelStatus.Available;
+    }
+
+    public void LockLevel()
+    {
+        this._status = LevelStatus.Locked;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+
+
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
---
+++ LevelSelection/Assets/Scripts/UILevelSelect.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILevelSelect : MonoBehaviour {
+
+
+    [SerializeField] private UILevel levelUI;
+    [SerializeField] private LevelPopup levelPopup;
+
+    private Transform levelSelectPanel;
+    private int currentPage;
+    private List<UILevel> levelList = new List<UILevel>();
+
+	// Use this for initialization
+	void Start () {
+        levelSelectPanel = transform;
+
+        for (int i = 0; i < LevelManager.Instance._allLevels.Count; i++)
+        {
+            levelList.Add(levelUI);
+        }
+        BuildLevelPage(0);
+    }
+	
+    void BuildLevelPage(int page)
+    {
+        RemoveItemsFromPage();
+        currentPage = page;
+        int pageSize = 12;
+        List<UILevel> pageList = levelList.Skip(page * pageSize).Take(pageSize).ToList();
+
+        for(int i = 0; i < pageList.Count; i++)
+        {
+            Level level = LevelManager.Instance._allLevels[(page * pageSize) + i];
+            UILevel instance = Instantiate(pageList[i]);
+            instance.SetStars(level.AmmountOfStars);
+            instance.transform.SetParent(levelSelectPanel);
+            instance.GetComponent<Button>().onClick.AddListener(() => SelectLevel(level));
+
+            if (level.Status != Level.LevelStatus.Locked)
+            {
+                instance.lockImage.SetActive(false);
+                instance.levelIDText.text = level.Id.ToString();
+            }
+            else
+            {
+                instance.lockImage.SetActive(true);
+                instance.levelIDText.text = "";
+            }
+        }
+    }
+
+    void RemoveItemsFromPage()
+    {
+        for (int i = 0; i < levelSelectPanel.childCount; i++)
+        {
+            Destroy(levelSelectPanel.GetChild(i).gameObject);
+        }
+    }
+
+    public void NextPage()
+    {
+        BuildLevelPage(currentPage + 1);
+    }
+
+    public void PreviousPage()
+    {
+        BuildLevelPage(currentPage - 1);
+    }
+
+    void SelectLevel(Level level)
+    {
+        if (level.Status == Level.LevelStatus.Locked)
+        {
+            levelPopup.gameObject.SetActive(true);
+            levelPopup.SetText("<b>Level " + level.Id + " is currently locked.</b>\nComplete level " + (level.Id-1) + " to unlock it!");
+            Debug.Log("Level locked.");
+        }
+        else
+        {
+            Debug.Log("Go to level: " + level.Id);
+            LevelManager.Instance.LoadSpecificLevel (level);
+        }
+    }
+}
